1fa4618613c558f046d3889a9a33f29779c91108
Sangeeth S Devan <sangeethsdevan@gmail.com>
2024-01-25 15:18:28 +0530

    script.js update

--- script.js
+++ script.js
@@ -18,16 +18,15 @@ navigator.geolocation.getCurrentPosition(sucess,error)
 async function getWeather(longitude,latitude){
   let data= await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=701a799e3d7e55b85f203bf41968344b`)
   let response = await data.json();
-  document.getElementById("temp").innerHTML=response.main.temp;
+  document.getElementById("temp").innerHTML=response.main.feels_like;
   document.getElementById("condition").innerHTML=response.weather[0].description
 
+  console.log(response)
 
   document.getElementById("humidity").innerHTML=response.main.humidity;
   document.getElementById("wind").innerHTML=response.wind.speed;
 
   const condition = response.weather[0].main;
-  
-
 
   if(condition==="Clear"){
     document.getElementById("condition-pic").src="./Assets/clear.png"
@@ -39,8 +38,6 @@ async function getWeather(longitude,latitude){
     document.getElementById("condition-pic").src="./Assets/cloud.png"
   }else if(condition==="Drizzle"){
     document.getElementById("condition-pic").src="./Assets/drizzle.png"
-  }else{
-
   }
 
 }
