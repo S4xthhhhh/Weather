455da174a2683284ac7d26adc00492f83895875e
Sangeeth S Devan <sangeethsdevan@gmail.com>
2024-01-29 22:02:33 +0530

    bug fix

--- script.js
+++ script.js
@@ -19,12 +19,14 @@ async function getWeather(longitude, latitude) {
     `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=701a799e3d7e55b85f203bf41968344b`
   );
   let response = await data.json();
+
   document.getElementById("temp").innerHTML = response.main.feels_like;
   document.getElementById("condition").innerHTML =
-    response.weather[0].description;
+    response.weather[0].main;
 
   document.getElementById("humidity").innerHTML = response.main.humidity;
   document.getElementById("wind").innerHTML = response.wind.speed;
+  document.getElementById("place").innerHTML = response.name;
 
   const condition = response.weather[0].main;
 
@@ -51,11 +53,12 @@ async function getQuote() {
 async function getWeatherbyCity() {
   let cityName = document.getElementById("search-input").value;
   let data = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}&appid=701a799e3d7e55b85f203bf41968344b`
+    `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName.trim()}&appid=701a799e3d7e55b85f203bf41968344b`
   );
   let response = await data.json();
 
   if (response.cod == 404) {
+    
   } else {
     document.getElementById("temp").innerHTML = response.main.feels_like;
     document.getElementById("condition").innerHTML =
